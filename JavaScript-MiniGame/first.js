42194539c2f6eae772a2203b0052388facbb1839
Patrycja <64614174+H0N3YY@users.noreply.github.com>
2023-03-28 10:28:41 +0200

    2019

---
+++ JavaScript-MiniGame/first.js
@@ -0,0 +1,144 @@
+var losowanie = Math.round(Math.random() * 20)
+
+function Minigame(rozgrywka) {
+
+
+    console.log("_________________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+    console.log("______________¶¶¶¶¶__¶__________________________¶¶");
+    console.log("___________¶¶¶¶__¶¶__¶___________________________¶");
+    console.log("_________¶¶¶_¶¶__¶__¶¶¶__________________________¶");
+    console.log("______¶¶¶_¶¶_¶¶¶_¶_¶¶_¶¶_________¶_______________¶");
+    console.log("_____¶_¶¶__¶_¶_¶¶¶¶_¶¶¶__________¶¶______________¶");
+    console.log("___¶¶¶_¶¶¶¶¶_¶¶¶¶¶¶_¶¶¶_________¶¶¶______________¶");
+    console.log("_¶¶__¶¶¶¶¶¶¶¶_¶¶_¶¶____________¶¶¶¶¶_____________¶");
+    console.log("¶_¶¶__¶__¶¶¶¶____¶¶___________¶¶¶¶¶¶¶____________¶");
+    console.log("¶__¶¶¶¶¶¶¶¶¶¶____¶¶__________¶¶¶¶¶¶¶¶¶¶__________¶");
+    console.log("_¶¶¶_¶_¶¶___¶¶___¶¶________¶¶¶¶¶¶¶¶¶¶¶¶¶_________¶");
+    console.log("__¶¶_¶¶_¶___¶¶___¶¶______¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_______¶");
+    console.log("___¶¶____¶___¶___¶¶____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_____¶");
+    console.log("____¶¶___¶¶__¶¶__¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___¶");
+    console.log("_____¶¶___¶__¶¶__¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶__¶");
+    console.log("______¶¶___¶__¶__¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶");
+    console.log("_______¶¶__¶¶_¶__¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶");
+    console.log("________¶¶__¶_¶¶_¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_¶");
+    console.log("_________¶¶__¶_¶_¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶__¶");
+    console.log("__________¶¶_¶¶¶_¶¶___¶¶¶¶¶¶¶¶¶__¶¶__¶¶¶¶¶¶¶¶¶___¶");
+    console.log("____________¶_¶¶_¶¶_____¶¶¶¶¶____¶¶____¶¶¶¶¶_____¶");
+    console.log("_____________¶_¶¶¶¶___________¶¶¶¶¶¶¶¶___________¶");
+    console.log("______________¶¶¶¶¶__________¶¶¶¶¶¶¶¶¶¶______¶¶__¶");
+    console.log("_______________¶¶¶____________¶¶¶¶¶¶¶¶_______¶¶¶_¶");
+    console.log("________________¶¶__________________________¶¶_¶_¶");
+    console.log("_________________¶¶__________________________¶¶__¶");
+    console.log("_________________¶¶__________________________¶¶¶_¶");
+    console.log("__________________¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+    console.log("__________________¶¶¶¶¶¶¶¶¶¶¶¶");
+    console.log("_____________________¶¶¶¶¶¶    ");
+
+
+
+
+
+    console.log(" ");
+    console.log("-.-.-.--.-.-.-.-.-.-.--.-.-.-.-.-.--.-");
+
+    console.log("ZASADY GRY");
+
+    console.log("★ tym więcej żetonów = szansa  na przeżycie w postapokaliptycznym świecie");
+    console.log("★ Do rozpoczęcia gry potrzebujesz jedynie kalkulatora, aby na końcu podsumować ilość zebranych żetonów!");
+    console.log("★ A i na wstępie masz 40 żetonów");
+
+
+    console.log(">-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+    console.log(" ");
+console.log("★ ★ ★  WPROWADZENIE ★ ★ ★ ");
+    
+    console.log(" Żyjesz na świecie gdzie nie zostało dosłownie nic z normalnej cywilizacji. Ludzie pozmieniali się w potwory, a jeszcze inni stali się tyranami gorszymi od tych pierwszych. Przemierzasz przez świat razem ze swoją grupą, z którą naprawdę wiele przeżyłeś. Twoim zadaniem jest przetrwać i utrzymać tych ludzi przy życiu jako lider. Każdego dnia spotyka was co innego jednak teraz pytanie co będzie dalej? ");
+
+
+
+    console.log(" ");
+    console.log(">-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+
+    console.log(" ");
+
+
+    console.log(" CO SIĘ STAŁO DZISIEJSZEGO DNIA? ");
+
+    console.log(" ");
+
+    console.log(">-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+
+    switch (rozgrywka) {
+
+        case 0:
+            console.log("Napadła na ciebie wroga grupa |TRACISZ -5 żetonów| ");
+            break;
+        case 1:
+            console.log("Znalazłeś pożywienie dla swojej grupy | OTRZYMUJESZ + 20 żetonów| ");
+            break;
+        case 2:
+            console.log("Znalazłeś porzucone sprawne auto |OTRZYMUJESZ + 14 żetonów| ");
+            break;
+        case 3:
+            console.log("Utknąłeś w mieście przez horde zombie | TRACISZ -10 żetonów| ");
+            break;
+        case 4:
+            console.log("Przeszedłeś kolejne mile swojej drogi bez przerwy |OTRZYMUJESZ + 30| ");
+            break;
+        case 5:
+            console.log("Jeden z twoich ludzi został ugryziony przez zombie, a gorączka z każdą chwilą staje sie nie do zniesienia. | TRACISZ -50 żetonów");
+            break;
+        case 6:
+            console.log("Pomogłeś obcej grupie wydostać się z zatrzasku | OTRZYMUJESZ + 40 żetonów| ");
+            break;
+        case 7:
+            console.log("Masz szczęście! Znalazłeś tymczasowe schronienie dla wszystkich | OTRZYMUJESZ + 100 żetonów |");
+            break;
+
+        case 8:
+            console.log(" Jeden z twoich ludzi potknął się i uszkodził swoją nogę |TRACISZ -11 żetonów |");
+            break;
+
+        case 9:
+            console.log(" Uciekłeś przed hordą zombie |OTRZYMUJESZ + 2 żetony |");
+            break;
+        case 10:
+            console.log("Przegrałeś walkę z wrogą grupą przez co straciłeś część swoich ludzi | TRACISZ -17 żetonów| ");
+            break;
+
+        case 11:
+            console.log("Zdobyłeś duży zapas broni/amunicji | OTRZYMUJESZ + 80 żetonów| ");
+            break;
+        case 12:
+            console.log(" Zraniłeś się podczas walki | TRACISZ - 4 żetonów| ");
+            break;
+        case 13:
+            console.log("Do twojej grupy dołączył wierny kompan, który jest psem | OTRZYMUJESZ +3 I TRACISZ -25| ");
+            break;
+        case 14:
+            console.log("Znalazłeś benzyne potrzebną do przebycia dalszej drogi autem | OTRZYMUJESZ + 11 żetonów| ");
+            break;
+        case 15:
+            console.log("Znalazłeś cukierki | OTRZYMUJESZ + 1 żetonów| ");
+            break;
+        case 16:
+            console.log(" Na swojej drodze spotkałeś osierocone niemowle, więc przyjmujesz je do grupy | OTRZYMUJESZ + 15 TRACISZ -45 żetonów| ");
+            break;
+
+        case 17:
+            console.log(" Jeden z twoich ludzi zgubił torbe z pożywieniem podczas wyprawy |TRACISZ -19 żetonów| ");
+            break;
+
+        case 18:
+            console.log("Zachorowałeś, a nie posiadacie przy sobie żadnych leków | TRACISZ -3 żetony| ");
+            break;
+        case 19:
+            console.log(" Dzień był spokojny i nic takiego się nie wydażyło  | NIC NIE OTRZYMUJESZ I NIE TRACISZ| ");
+            break;
+
+    }
+
+}
+
+
+Minigame(losowanie);
